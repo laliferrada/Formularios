5b0b4b60c4d618df3a4c8f688b8bad227f180f7d
Laura <ferradalaura14@gmail.com>
2018-10-11 22:39:14 -0300

    Initial commit

---
+++ app/src/main/java/google/com/ar/formularios/MainActivity_FormaDePago.java
@@ -0,0 +1,13 @@
+package google.com.ar.formularios;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity_FormaDePago extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main__forma_de_pago);
+    }
+}
---
+++ app/src/main/java/google/com/ar/formularios/MainActivity_busqueda.java
@@ -0,0 +1,13 @@
+package google.com.ar.formularios;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity_busqueda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_busqueda);
+    }
+}
